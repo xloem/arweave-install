8b52e2c136d3c9f121f93aa66822634b3f1deef2
John Doe <johndoe@example.com>
2024-12-27 11:16:32 -0500

    llm: change service to openrouter, change service url schema to provide for model paths containing / character

--- ai/llm.py
+++ ai/llm.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import openai as llm
+import logging
 
 class Inference:
     def __init__(self, service):
@@ -13,37 +14,47 @@ class Inference:
                 filecontent = fileobj.read()
             system_prompt += file_prefix.format(filename=filename) + filecontent + file_suffix.format(filename=filename)
         system_prompt += system_prompt_suffix
-        response = self.llm.chat.completions.create(
-            model = self.model,
-            messages = [
-                dict(role='system', content=system_prompt),
-                dict(role='user', content=prompt),
-            ],
-            **kwparams,
-            stream = True,
-        )
-        doc = ''
-        for update in response:
-            text = update.choices[0].delta.content
-            print(text, end='', flush=True)
-            doc += text
-        return doc
+        while True:
+            try:
+                response = self.llm.chat.completions.create(
+                    model = self.model,
+                    messages = [
+                        dict(role='system', content=system_prompt),
+                        dict(role='user', content=prompt),
+                    ],
+                    **kwparams,
+                    stream = True,
+                )
+                doc = ''
+                for update in response:
+                    text = update.choices[0].delta.content
+                    print(text, end='', flush=True)
+                    doc += text
+                return doc
+            except llm.APIError as err:
+                logging.warning(err.body)
+                if err.body['code'] == 429:
+                    logging.warning('trying again, this may take a long time')
+                    continue
+                raise
 
 def service_str_to_url_key_model(service):
     spliturl = urllib.parse.urlsplit(service)
-    model = spliturl.username
-    key = spliturl.password
+    key = spliturl.username or spliturl.password
+    model = spliturl.fragment
     scheme, netloc, path, query, fragment = spliturl
-    netloc = netloc.split('@',1)[1]
-    url = urllib.parse.urlunsplit([scheme, netloc, path, query, fragment])
+    url = urllib.parse.urlunsplit([scheme, netloc.split('@',1)[1], path, query, ''])
     return url, key, model
+
+SERVICE_SAMBANOVA_LLAMA31 = 'https://f47ef722-8eea-4f95-8d57-02027bdc9401@api.sambanova.ai/v1#Meta-Llama-3.1-405B-Instruct'
+SERVICE_OPENROUTER_LLAMA31 = 'https://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@openrouter.ai/api/v1#meta-llama/llama-3.1-405b-instruct:free'
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--files', action='append', nargs='*', required=True)
     parser.add_argument('--prompt', required=True)
-    parser.add_argument('--service', default='https://Meta-Llama-3.1-405B-Instruct:f47ef722-8eea-4f95-8d57-02027bdc9401@api.sambanova.ai/v1')
+    parser.add_argument('--service', default=SERVICE_OPENROUTER_LLAMA31)
     parser.add_argument('--temperature', default=0)
     parser.add_argument('--max-tokens', default=1024)
     args = parser.parse_args()
